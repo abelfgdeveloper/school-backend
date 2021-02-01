a3013b6966a918b2cba735eb63e4e3fb9374a87e
Abel <abeldevelop@users.noreply.github.com>
2021-02-01 23:46:17 +0100

    Create the eureka-server

---
+++ school-server-parent/school-eureka-server/src/main/java/es/abelfgdeveloper/school/server/SchoolEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package es.abelfgdeveloper.school.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SchoolEurekaServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SchoolEurekaServerApplication.class, args);
+  }
+}
