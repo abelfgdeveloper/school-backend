bba05a57549691b2fd6c0da9548e0cadbc2e9cfc
Abel <abeldevelop@users.noreply.github.com>
2021-02-10 00:55:02 +0100

    school-common: try to configure logbook for restTemplate

--- school-library-parent/school-common/src/main/java/es/abelfgdeveloper/common/config/communication/CommunicationBeans.java
+++ school-library-parent/school-common/src/main/java/es/abelfgdeveloper/common/config/communication/CommunicationBeans.java
@@ -1,21 +1,26 @@
 package es.abelfgdeveloper.common.config.communication;
 
 import java.time.Duration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
+@RequiredArgsConstructor
 @Configuration
 public class CommunicationBeans {
 
-  @Primary
   @Bean
-  public RestTemplate defaultRestTemplate(RestTemplateBuilder builder) {
+  @LoadBalanced
+  public RestTemplate defaultRestTemplate(
+      RestTemplateBuilder builder, LogbookClientHttpRequestInterceptor interceptor) {
     return builder
         .setConnectTimeout(Duration.ofSeconds(3))
         .setReadTimeout(Duration.ofSeconds(3))
+        //        .additionalInterceptors(interceptor)
         .build();
   }
 }
