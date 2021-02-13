a56b8dfa6c7a6a506879ea1befe6796bc1e886df
Abel <abeldevelop@users.noreply.github.com>
2021-02-13 01:09:43 +0100

    school-students-service-rest-client: take service url from properties

--- school-service-parent/school-students-parent/school-students-service-rest-client/src/main/java/es/abelfgdeveloper/school/service/students/client/v1/impl/DefaultStudentsClient.java
+++ school-service-parent/school-students-parent/school-students-service-rest-client/src/main/java/es/abelfgdeveloper/school/service/students/client/v1/impl/DefaultStudentsClient.java
@@ -3,6 +3,7 @@ package es.abelfgdeveloper.school.service.students.client.v1.impl;
 import es.abelfgdeveloper.school.service.students.api.v1.resource.response.StudentResponseResource;
 import es.abelfgdeveloper.school.service.students.client.v1.StudentsClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,14 +11,19 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class DefaultStudentsClient implements StudentsClient {
 
-  private static final String BASE_URL = "http://school-students-service/students/v1";
+  @Value("${es.abelfgdeveloper.service.school.students}")
+  private String baseUrl;
+
+  private static final String BASE_PATH = "/students/v1";
+
   private static final String SLASH = "/";
 
   private final RestTemplate restTemplate;
 
   @Override
   public StudentResponseResource findById(String id) {
-    String url = new StringBuilder().append(BASE_URL).append(SLASH).append(id).toString();
+    String url =
+        new StringBuilder().append(baseUrl).append(BASE_PATH).append(SLASH).append(id).toString();
     return restTemplate.getForObject(url, StudentResponseResource.class);
   }
 }
