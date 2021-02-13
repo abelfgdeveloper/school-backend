bd40e37c4366157825f4c1337e1090409aef6cf5
Abel <abeldevelop@users.noreply.github.com>
2021-02-13 01:08:29 +0100

    school-common: CommunicationBeans not for test profile

--- school-library-parent/school-common/src/main/java/es/abelfgdeveloper/common/config/communication/CommunicationBeans.java
+++ school-library-parent/school-common/src/main/java/es/abelfgdeveloper/common/config/communication/CommunicationBeans.java
@@ -6,9 +6,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
+@Profile("!test")
 @RequiredArgsConstructor
 @Configuration
 public class CommunicationBeans {
